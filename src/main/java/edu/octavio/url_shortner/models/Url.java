d0d40c48847d9318628d790fd6750dc1b8af8435
Octavio Piratininga <59701790+Pira4Ever@users.noreply.github.com>
2024-09-16 13:51:24 -0300

    feat: added no args constructor to Url

--- src/main/java/edu/octavio/url_shortner/models/Url.java
+++ src/main/java/edu/octavio/url_shortner/models/Url.java
@@ -3,12 +3,14 @@ package edu.octavio.url_shortner.models;
 import edu.octavio.url_shortner.dtos.UrlDto;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "urls")
 @Getter
 @Setter
+@NoArgsConstructor
 public class Url {
     @Id
     private String id;
